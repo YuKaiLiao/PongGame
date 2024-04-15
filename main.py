149e3ebfea3f52528582344f29c4d4d154a5bb77
LiaoYK <lucky5877112@gmail.com>
2024-04-15 23:00:53 +0800

    new project and set the screen

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+from turtle import Screen
+
+GameScreen = Screen()
+GameScreen.setup(800, 600)
+GameScreen.bgcolor("black")
+GameScreen.title("Pong")
+
+GameScreen.exitonclick()
